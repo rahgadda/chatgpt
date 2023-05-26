668baa67b99c9e69d59297fcd5425099802ad89c
rahgadda <gaddam.rahul@gmail.com>
2023-05-26 06:51:07 +0000

    Initial Draft

---
+++ 02-modules/05-gradio/gradio.md
@@ -0,0 +1,7 @@
+# Gradio
+
+## Installation
+```bash
+cd 02-modules/05-gradio
+pip  install -r requirements.txt
+```
--- 02-modules/05-gradio/install.sh
+++ 02-modules/05-gradio/install.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# -- Running Weaviate DB
-# -- Review data using >> https://console.semi.technology/
-docker run -it --rm --name weaviate -e "OPENAI_APIKEY=XX" -e "QUERY_DEFAULTS_LIMIT=25" -e "AUTHENTICATION_ANONYMOUS_ACCESS_ENABLED=true" -e "PERSISTENCE_DATA_PATH=/var/lib/weaviate" -e "DEFAULT_VECTORIZER_MODULE=text2vec-openai" -e "ENABLE_MODULES=text2vec-openai,generative-openai" -e "CLUSTER_HOSTNAME=node1" -p 8080:8080 -v $(pwd):/weaviate-data semitechnologies/weaviate:latest
-
-## -- Testing
-curl -X GET http://localhost:8080/v1/meta
-
-# -- Text Extractor
-pip install fastapi
-pip install uvicorn
-pip install requests
-pip install pdfminer
-pip install weaviate-client
-pip install python-multipart
-
-pip install gradio
-
-uvicorn fileUpload:app --reload
+cd 02-modules/05-gradio
+pip  install -r requirements.txt
