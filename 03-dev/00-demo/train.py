b8543e60436ecf707af671a90cead885bcec5faa
rahgadda <gaddam.rahul@gmail.com>
2023-05-28 15:31:41 +0000

    Initial Draft

--- 03-dev/00-demo/train.py
+++ 03-dev/00-demo/train.py
@@ -39,7 +39,7 @@ g_product_description=""
 g_product_prompt=""
 g_output=""
 g_weaviate_url=""
-client=None
+g_client=None
 
 ############################
 ###### Generic Code #######
@@ -114,11 +114,11 @@ def update_global_variables(ui_api_key, ui_weaviate_url, ui_product_name, ui_pro
 
 # -- Create Weaviate Connection
 def weaviate_client():
-    global client
+    global g_client
     global g_output
 
     try:
-        client = Client(url=g_weaviate_url, timeout_config=(3.05, 9.1))
+        g_client = Client(url=g_weaviate_url, timeout_config=(3.05, 9.1))
         print("Weaviate client connected successfully!")
         g_output=g_output+"Weaviate client connected successfully!"
     except Exception as e:
@@ -128,6 +128,136 @@ def weaviate_client():
 ############################
 ##### Create Product DB ####
 ############################
+
+# -- Check for Product Table
+def create_product_class():
+    global g_client
+    global g_output
+
+    print("started function - create_product_class")
+
+    # Define the class "Product" with properties name,description
+    product_class = {
+                        "classes": [{
+                            "class": "Product",
+                            "description": "Store Product Names and Description",
+                            "vectorizer": "none",
+                            "properties": [
+                                {
+                                    "name": "name",
+                                    "dataType": ["text"],
+                                    "description": "Product Name"
+                                },
+                                {
+                                    "name": "description",
+                                    "dataType": ["text"],
+                                    "description": "Product Description"
+                                },
+                                {
+                                    "name": "prompt",
+                                    "dataType": ["text"],
+                                    "description": "Prompt variable to store mapping description. This is non-mandatory"
+                                }, 
+                                {
+                                    "name": "um_indicator",
+                                    "dataType": ["text"],
+                                    "description": "Indicator to check in User Manual exist"
+                                }   
+                            ]
+                        }]
+                    }
+
+    # Create the class in Weaviate
+    try:
+        response = g_client.schema.create(product_class)
+        g_output=g_output+"Class 'Product' created successfully!\n"
+        print("Class 'Product' created successfully!")
+    except Exception as e:
+        g_output=g_output+f"Failed to create class 'Product': {e}"+"\n"
+        print(f"Failed to create class 'Product': {e}")
+    finally:
+        print("completed function - create_product_class")
+
+# -- Check for Product Object
+def validate_product_object_exist():
+    global g_client
+    global g_product_name
+    global g_output
+
+    print("started function - validate_product_object_exist")
+
+    # Check if data exists based on input - product_name  
+    where_filter = {
+                        "path": ["name"],
+                        "operator": "Equal",
+                        "valueString": g_product_name
+                   }
+
+    query_result = (
+                        g_client.query
+                        .get("Product", "name")
+                        .with_where(where_filter)
+                        .do()
+                   )
+    
+    print("Product Table Query Result - "+str(query_result))
+    if len(query_result['data']['Get']['Product']) == 0:
+        g_output=g_output+"Product object does not exists\n"
+        print("completed function - validate_product_object_exist")
+        return True
+    else:
+        g_output=g_output+"Product object already exists\n"
+        print("completed function - validate_product_object_exist")
+        return False
+
+# -- Create new Product Object
+def create_new_product_object():
+    global g_client
+    global g_product_name
+    global g_product_description
+    global g_product_prompt
+    global g_output
+
+    print("started function - create_new_product_object")
+    try:
+        data_object =   {
+                            "name": g_product_name,
+                            "description": g_product_description,
+                            "prompt": g_product_prompt,
+                            "um_indicator": 'N'
+                        }
+
+        g_client.data_object.create(data_object, class_name="Product")
+        print("Product object Created Successfully")
+        g_output=g_output+"Product object Created Successfully\n"
+    except Exception as e:
+        raise ValueError("Creating Product Object"+str(e))
+    finally:
+        print("completed function - create_new_product_object")
+
+# -- Add Product Object
+def add_product_data():
+    global g_product_name
+    global g_product_description
+    global g_client
+    global g_output
+
+    print("started function - add_product_data")
+
+    # -- Check if Product Table Exist
+    try:
+        g_client.schema.get("Product")
+        print("Class 'Product' already exists!")
+        g_output=g_output+"Class 'Product' already exists!\n"
+    except Exception as e:
+        print(f"Error Verifying Class Product : {e}")
+        create_product_class()
+
+    # -- Check & Create new Product Object
+    if validate_product_object_exist():
+       create_new_product_object() 
+    
+    print("completed function - add_product_data")
 
 ############################
 ##### Create Product UM ####
@@ -159,6 +289,14 @@ def submit(ui_api_key, ui_weaviate_url, ui_product_name, ui_product_description,
             weaviate_client()
             g_output=g_output+"\n>>> 2 - Completed <<<\n"
             print(">>> 2 - Completed <<<\n")
+
+            # Create Product Class & Object
+            g_output=g_output+"\n>>> 3 - Create Product Class & Object <<<\n"
+            print(">>> 3 - Create Product Class & Object <<<")
+            add_product_data()
+            g_output=g_output+">>> 3 - Completed <<<\n"
+            print(">>> 3 - Completed <<<\n")
+
         except Exception as e:
             print(">>> Completed Training <<<\n")
             return g_output+"Error -> " + str(e)
