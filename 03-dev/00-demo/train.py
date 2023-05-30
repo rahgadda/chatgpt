2b5d31388f8c37e1abb7b7a4632d3c4d506c1078
rahgadda <gaddam.rahul@gmail.com>
2023-05-30 05:06:44 +0000

    Initial Draft

--- 03-dev/00-demo/train.py
+++ 03-dev/00-demo/train.py
@@ -5,6 +5,10 @@ from weaviate.client import Client
 from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 import tempfile
+import openai
+from openai.embeddings_utils import get_embedding
+import pandas as pd
+from bs4 import BeautifulSoup
 
 ############################
 ### Variable Declaration ###
@@ -73,6 +77,7 @@ def update_global_variables(ui_api_key, ui_weaviate_url, ui_product_name, ui_pro
         if ui_api_key != "":
             print('Setting g_openai_api_key - '+ui_api_key)
             g_openai_api_key=ui_api_key
+            openai.api_key=g_openai_api_key
             g_output=g_output+'Setting g_openai_api_key - '+ui_api_key
         else:
             print("exception in function - update_global_variables")
@@ -138,7 +143,7 @@ def convert_to_camel_case(string):
 
 # -- Create OpenAI Embedding
 def create_openai_embeddings(text):
-    print("Creating embedding for text"+ text)
+    # print("Creating embedding for text"+ text)
 
     # Updating Embedding
     retry_attempts = 3
@@ -152,6 +157,26 @@ def create_openai_embeddings(text):
         except Exception as e:
             time.sleep(retry_interval)
             print(str(e))
+
+# -- Generate OpenAI Description
+def generate_openAI_description(key,prompt):
+
+    text = prompt.replace('{text}', key)
+
+    # Generate text using the OpenAI model
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=text,
+        max_tokens=1000
+    )
+
+    openai_data = response.choices[0].text.strip()
+
+    # Extract text from HTML using BeautifulSoup
+    soup = BeautifulSoup(openai_data, 'html.parser')
+    clean_text = soup.get_text(separator=' ')
+
+    return clean_text
 
 ############################
 ##### Create Product DB ####
@@ -416,59 +441,58 @@ def extract_text_from_pdf(file):
 
     # Text Splitter
     text_splitter = CharacterTextSplitter(    
-        chunk_size = 3500,
+        chunk_size = 1000,
         chunk_overlap  = 0,
         length_function = len,
     )
 
     # Read the PDF file page by page
-    item = {}
-    with open(file_path, "rb") as pdf_file:
-        pdf = PdfReader(pdf_file)
-        for page_no, page in enumerate(pdf.pages, start=1):
-            text = page.extract_text()
+    try:
+        item = {}
+        with open(file_path, "rb") as pdf_file:
+            pdf = PdfReader(pdf_file)
+            for page_no, page in enumerate(pdf.pages, start=1):
+                text = page.extract_text()
 
-            # Merge hyphenated words
-            text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+                # Merge hyphenated words
+                text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
 
-            # Fix newlines in the middle of sentences
-            text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+                # Fix newlines in the middle of sentences
+                text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
 
-            # Remove multiple newlines
-            text = re.sub(r"\n\s*\n", "\n\n", text)
-            
-            print('Processing Page Content - '+str(page_no))
+                # Remove multiple newlines
+                text = re.sub(r"\n\s*\n", "\n\n", text)
+                
+                print('Processing Page Content - '+str(page_no))
 
-            if text:
-                # Split the text into smaller chunks
-                chunks = text_splitter.split_text(text)
+                if text:
+                    # Split the text into smaller chunks
+                    chunks = text_splitter.split_text(text)
 
-                # Process each chunk individually
-                for chunk in chunks:
-                    item =  {
-                                'text': chunk,
-                                'embedding': create_openai_embeddings(chunk),
-                                'page_no': page_no
-                            }
-                    
-                    create_new_um_object(item)
-                    
-    
+                    # Process each chunk individually
+                    for chunk in chunks:
+                        item =  {
+                                    'text': chunk,
+                                    'embedding': create_openai_embeddings(chunk),
+                                    'page_no': page_no
+                                }
+                        
+                        create_new_um_object(item)
+    except Exception as e:
+        raise ValueError(str(e))
+
     print("completed function - extract_text_from_pdf")
-
 
 # -- Process User Manual
 def process_um_data(file):
     
-    # If um table/class exists, system will delete and recreate
+    # If um table/class exists, system will delete and recreate   
     # if validate_um_object_exist():
     #     delete_um_class()
-    # else:    
-    #     create_um_class()
-
-    extract_text_from_pdf(file)
-
-
+    
+    if not(validate_um_object_exist()):
+        create_um_class()
+        extract_text_from_pdf(file)
 
 ############################
 #### Create Product Map ####
@@ -476,19 +500,179 @@ def process_um_data(file):
 
 # -- Check for Mapping Class/Table
 def create_mapping_class():
-    None
+    global g_product_name
+    global g_client
+    global g_output
+
+    print("started function - create_mapping_class")
+    product_class_name_camel_case = convert_to_camel_case(str(g_product_name+"_mapping"))
+    print("Creating Mapping Artefact of "+product_class_name_camel_case)
+
+    # Define the class with `ProductMapping` to store user manual details
+    product_mapping =    {
+                        "classes": [{
+                            "class": product_class_name_camel_case,
+                            "description": "Vector store of "+g_product_name+" mapping",
+                            "vectorizer": "none",
+                            "properties": [
+                                {
+                                    "name": "key",
+                                    "dataType": ["text"],
+                                    "description": "Key Column"
+                                },
+                                {
+                                    "name": "description",
+                                    "dataType": ["text"],
+                                    "description": "Description of Master Table Key Data"
+                                }  
+                            ]
+                        }]
+                    }
+    
+    # Create the class in Weaviate
+    try:
+        response = g_client.schema.create(product_mapping)
+        g_output=g_output+"Class '"+product_class_name_camel_case+"' created successfully!\n"
+        print("Class '"+str(product_mapping)+"' created successfully!")
+    except Exception as e:
+        g_output=g_output+f"Failed to create class '"+str(product_mapping)+"': {e}"+"\n"
+        print(f"Failed to create class '"+str(product_mapping)+"': {e}")
+        raise ValueError(str(e))
+    finally:
+        print("completed function - create_mapping_class")
+
+# -- Check for Mapping Class/Table
+def delete_mapping_class():
+    global g_client
+    global g_product_name
+    global g_output
+
+    print("started function - delete_mapping_class")
+    product_class_name_camel_case = convert_to_camel_case(str(g_product_name+"_mapping"))
+
+    try:
+        g_client.schema.delete_class(product_class_name_camel_case)
+        print("Class "+product_class_name_camel_case+" deleted successfully.")
+        g_output=g_output+"Class "+product_class_name_camel_case+" deleted successfully.\n"
+    except Exception as e:
+        print(f"Failed to delete class: {e}")
+        g_output=g_output+f"Failed to delete class: {e}"+"\n"
+        raise ValueError(str(e))
+    finally:
+        print("completed function - delete_mapping_class")
 
 # -- Check for Mapping Object/Row
 def validate_mapping_object_exist():
-    None
+    global g_client
+    global g_product_name
+    global g_output
+    return_val=False
+
+    print("started function - validate_mapping_object_exist")
+    product_class_name_camel_case = convert_to_camel_case(str(g_product_name+"_mapping"))
+
+    try:
+        schema = g_client.schema.get()
+        classes = schema['classes']
+
+        # Check if the class exists in the schema
+        if any(cls['class'] == product_class_name_camel_case for cls in classes):
+            g_output=g_output+"Class "+product_class_name_camel_case+" exists in Weaviate.\n"
+            print("Class "+product_class_name_camel_case+" exists in Weaviate.")
+            return_val = True
+        else:
+            g_output=g_output+"Class "+product_class_name_camel_case+" does not exists in Weaviate.\n"
+            print("Class "+product_class_name_camel_case+" does not exist in Weaviate.")
+
+    except Exception as e:
+        g_output=g_output+f"Failed to retrieve schema: {e}"+"\n"
+        print(f"Failed to retrieve schema: {e}"+"\n")
+        raise ValueError(str(e))
+    finally:
+        print("completed function - validate_mapping_object_exist")
+        return return_val
 
 # -- Create new Mapping Object/Row
-def create_new_mapping_object():
-    None
+def create_new_mapping_object(item):
+    global g_client
+    global g_product_name
+
+    print("started function - create_new_mapping_object")
+    print("Storing mapping data into Weaviate")
+
+    data_object = {
+                        "key": item['key'],
+                        "description": item['description']
+                  }
+    try:
+        # Add the object to Weaviate
+        g_client.data_object.create(data_object, class_name=convert_to_camel_case(str(g_product_name+"_mapping")),vector=item['embedding'])
+    except Exception as e:
+        print("Error storing mapping record/object")
+        raise ValueError(str(e))
+    finally:
+        print("completed function - create_new_mapping_object")
+
+# -- Extract text from Excel Mapping File
+def extract_text_from_xlsx(file):
+    global g_product_prompt
+
+    file_path = file.name
+
+    print("started function - extract_text_from_xlsx")
+    print("Uploaded xlsx location - "+file_path)
+
+    try:
+         # Read all tabs from the Excel file into a dictionary of dataframes
+        dfs = pd.read_excel(file_path, sheet_name=None)
+
+        # Create an empty dictionary to store the combined values
+        combined_values = {}
+        
+        # Loop through each dataframe in the dictionary
+        for sheet_name, df in dfs.items():
+            # Get the column names and hints from the dataframe
+            column_names = df['Column Name'].tolist()
+            hints = df['Hint'].tolist()
+
+            # Combine the values and add them to the dictionary
+            combined_values.update({f"{sheet_name}.{column_names}": f"{column_names} {hint}" for column_names, hint in zip(column_names, hints)})
+
+        # Print the combined values
+        item={}
+        for key, value in combined_values.items():
+
+            print(f"Key: {key}")
+            print(f"Initial Value: {value}")
+
+            if g_product_prompt != "":
+                value=value+" "+generate_openAI_description(key,g_product_prompt)
+                print(f"Update Value: {value}")
+
+            print("-------------------------")
+            item= {
+                        'key':key,
+                        'description': value,
+                        'embedding': create_openai_embeddings(value)
+                }
+
+            create_new_mapping_object(item)
+    
+    except Exception as e:
+        raise ValueError(str(e))
+    finally:
+        print("completed function - extract_text_from_xlsx")
 
 # -- Process Mapping Excel Data
-def process_mapping_data():
-    None
+def process_mapping_data(file):
+    
+    # If um table/class exists, system will delete and recreate
+    if validate_mapping_object_exist():
+        delete_mapping_class()
+
+    if not(validate_mapping_object_exist()):
+        create_mapping_class()
+        extract_text_from_xlsx(file)
 
 ############################
 ###### Submit Button #######
@@ -530,6 +714,13 @@ def submit(ui_api_key, ui_weaviate_url, ui_product_name, ui_product_description,
             process_um_data(ui_product_um)
             g_output=g_output+">>> 4 - Completed <<<\n"
             print(">>> 4 - Completed <<<\n")
+
+            # Create Mapping Class & Object is file is inputted
+            g_output=g_output+"\n>>> 5 - Create Mapping Class & Object <<<\n"
+            print(">>> 5 - Create Mapping Class & Object <<<")
+            process_mapping_data(ui_product_mapping)
+            g_output=g_output+">>> 5 - Completed <<<\n"
+            print(">>> 5 - Completed <<<\n")
 
         except Exception as e:
             print("Error -> " + str(e))
